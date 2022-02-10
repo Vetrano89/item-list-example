e2408456049a1bb9eefb1fe7823d0f96f3a182f3
Vetrano89 <crvetrano@gmail.com>
2022-02-10 13:45:40 -0500

    barebones ListInputControl component to save list item state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Input from './components/input';
+import ListInputControl from './components/list-input-control';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <ListInputControl
+          label="Top 3 priorities"
+          placeholder="Enter your priorities here"
+          disabled={false}
+        />
+      </div>
     </div>
   );
 }
